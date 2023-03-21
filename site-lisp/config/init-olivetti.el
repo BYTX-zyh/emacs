48243e9ea44dc5904615e3808166cc2b9ad9e3c3
BYTX-zyh <18235499556@163.com>
2023-03-21 15:56:13 +0800

    add hook to olivetti

--- site-lisp/config/init-olivetti.el
+++ site-lisp/config/init-olivetti.el
@@ -26,4 +26,20 @@
                     (olivetti-mode 1)
                     (olivetti-set-width 120))))))
 
+
+(defun auto-olivetti ()
+  "检查当前是否为唯一窗口,根据其mode选择是否为其启动 olivetti"
+  (when (eq (length (window-list)) 1)
+      (olivetti-mode 1)
+      (olivetti-set-width 120)
+      ))
+
+(add-hook 'window-configuration-change-hook #'auto-olivetti)
+(dolist (hook (list
+               'org-mode-hook
+               'emacs-lisp-mode-hook
+               'c-ts-mode-hook
+               'c++-ts-mode-hook))
+  (add-hook hook #'auto-olivetti))
+
 (provide 'init-olivetti)
